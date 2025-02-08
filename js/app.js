44eaa3b3538ef71d0fe59ea0d94111230af3e47c
aminfo <aminfoprogramming@gmail.com>
2025-02-08 17:55:56 +0330

    aminfo upgrade to v2

--- js/app.js
+++ js/app.js
@@ -1,42 +1,57 @@
 // -------------------- Start DarkMode Handle -------------------- //
-var moon = document.querySelector('.moon')
-var sun = document.querySelector('.sun')
+var moon = document.querySelector(".moon");
+var sun = document.querySelector(".sun");
 
-if (localStorage.darkMode === 'true' || (!('darkMode' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    localStorage.setItem('darkMode', 'true')
-    document.documentElement.classList.add('dark')
-    document.documentElement.classList.remove('light')
-    moon.classList.add('hidden')
+if (
+  localStorage.darkMode === "true" ||
+  (!("darkMode" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  localStorage.setItem("darkMode", "true");
+  document.documentElement.classList.add("dark");
+  document.documentElement.classList.remove("light");
+  moon.classList.add("hidden");
 } else {
-    localStorage.setItem('darkMode', 'false')
-    document.documentElement.classList.remove('dark')
-    document.documentElement.classList.add('light')
-    sun.classList.add('hidden')
+  localStorage.setItem("darkMode", "false");
+  document.documentElement.classList.remove("dark");
+  document.documentElement.classList.add("light");
+  sun.classList.add("hidden");
 }
 
 function darkMode() {
-    return {
-        makeDark() {
-            if (localStorage.darkMode === 'false') {
-                localStorage.darkMode = 'true'
-                document.documentElement.classList.remove('light')
-                document.documentElement.classList.add('dark')
-                moon.classList.add('hidden')
-                sun.classList.remove('hidden')
-                sun.classList.add('block')
-            }
-        },
+  return {
+    makeDark() {
+      if (localStorage.darkMode === "false") {
+        localStorage.darkMode = "true";
+        document.documentElement.classList.remove("light");
+        document.documentElement.classList.add("dark");
+        moon.classList.add("hidden");
+        sun.classList.remove("hidden");
+        sun.classList.add("block");
+      }
+    },
 
-        makeLight() {
-            if (localStorage.darkMode === 'true') {
-                localStorage.darkMode = 'false'
-                document.documentElement.classList.remove('dark')
-                document.documentElement.classList.add('light')
-                sun.classList.add('hidden')
-                moon.classList.remove('hidden')
-                moon.classList.add('block')
-            }
-        }
-    }
+    makeLight() {
+      if (localStorage.darkMode === "true") {
+        localStorage.darkMode = "false";
+        document.documentElement.classList.remove("dark");
+        document.documentElement.classList.add("light");
+        sun.classList.add("hidden");
+        moon.classList.remove("hidden");
+        moon.classList.add("block");
+      }
+    },
+  };
 }
-        // -------------------- End DarkMode Handle -------------------- //
+// -------------------- End DarkMode Handle -------------------- //
+
+document
+  .getElementById("emailForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let subject = encodeURIComponent(document.getElementById("subject").value);
+    let body = encodeURIComponent(document.getElementById("body").value);
+
+    window.location.href = `mailto:your@email.com?subject=${subject}&body=${body}`;
+  });
